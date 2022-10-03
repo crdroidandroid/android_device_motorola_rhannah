ded3e67382c4e71c04f640115a2e06631e7ce7e6
5H4ND <miqueiasantos20@gmail.com>
2022-10-03 14:50:07 -0300

    rhannah: Force the use of msm8996-caf hals

--- device.mk
+++ device.mk
@@ -25,4 +25,7 @@ PRODUCT_COPY_FILES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/qcom-caf/msm8996/display \
+    hardware/qcom-caf/msm8996/audio \
+    hardware/qcom-caf/msm8996/media
